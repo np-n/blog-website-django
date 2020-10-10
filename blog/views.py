c64b3803214aa88bee6e682d34f1a964e6b6d256
nimesh44 <nimesh.neupane43@gmail.com>
2020-10-10 21:58:06 +0545

    Post model created and added to admin,database and migration

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Post
 
-posts = [
-            {
-                'author':'Netra Prasad Neupane',
-                'title':'Blog Post 1',
-                'content':'First blog post content',
-                'date_posted':'october 7 2020'
-            },
-            {
-                'author':'Nimesh Neupane',
-                'title':'Blog Post 2',
-                'content':'Second blog post content',
-                'date_posted':'october 8 2020'
-            },
 
-        ]
+
+# # Creating dummy data
+# posts = [
+#             {
+#                 'author':'Netra Prasad Neupane',
+#                 'title':'Blog Post 1',
+#                 'content':'First blog post content',
+#                 'date_posted':'october 7 2020'
+#             },
+#             {
+#                 'author':'Nimesh Neupane',
+#                 'title':'Blog Post 2',
+#                 'content':'Second blog post content',
+#                 'date_posted':'october 8 2020'
+#             },
+
+#         ]
 
 def home(request):
 
     # passing dicionary
     context = {
-        # Passing as posts variable
-        'posts' : posts
+
+        # Getting and passing actual post model from database
+        'posts' : Post.objects.all()
+
+        # # Passing  above dummy data as a dictionary
+        # 'posts' : posts
     }
     # return HttpResponse('<h1>Blog Home</h1>')
     # For second argument it looks subsirectory of templates inside app
