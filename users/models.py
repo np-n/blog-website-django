97aca2bec40ae1c7b26a5f41f87611f25406037b
nimesh44 <nimesh.neupane43@gmail.com>
2020-10-14 16:20:55 +0545

    update user profile, rendering user image alongside post in home page

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -10,3 +11,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} profile'
+
+
+# Not resized why?
+# This is run after model is saved to resize image
+def save(self, *args, **kwargs):
+    # running save method of parent class
+    # super().save()
+    super().save(*args, **kwargs)
+    img = Image.open(self.image.path)
+    if img.height > 300 or img.width > 300 :
+        output_size =(300,300)
+        # Resize image
+        img.thumbnail(output_size)
+        # save resized image to it's location/path
+        img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 # To add check point if user who are not logged in try to get profile
 # We will do it using decorators
 from django.contrib.auth.decorators import login_required
@@ -22,7 +22,7 @@ def register(request):
             # messages.success(request,f'Account created for {username}!')
             # # if submitted info are valid then we redirect users to home page
             # return redirect('blog-home')
-            messages.success(request,f'Hi! {username}, your account has been created! Now you are able to log in')
+            messages.success(request,f'Hi {username}, your account has been created! Now you are able to log in')
             return redirect('login')
 
 
@@ -33,4 +33,27 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    # If form submit method is POST then store post content in form variable
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance = request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance = request.user.profile)
+        # If all form input are valid, then send success message
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'Hi {request.user.username}, your account has been updated!')
+            return redirect('profile')
+
+
+
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+
+    # To pass both update form we will a dictionary
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+        }
+    return render(request,'users/profile.html',context)
